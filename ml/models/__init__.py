ea4bf0d6194aa068101d4ed3d65eb15b1c69c94a
Finlay Miller <finlay@dal.ca>
2024-09-11 13:59:36 -0300

    added clamp model

--- ml/models/__init__.py
+++ ml/models/__init__.py
@@ -1,6 +1,7 @@
 from .bad_dae import *
 from .base import *
+from .clamp import *
 from .vanilla_vae import *
 from .vq_vae import *
 
-vae_models = {"BadDAE": BadAutoEncoder, "VanillaVAE": VanillaVAE, "VQVAE": VQVAE}
+vae_models = {"BadDAE": BadAutoEncoder, "VanillaVAE": VanillaVAE, "VQVAE": VQVAE, "CLaMP": CLaMP}
